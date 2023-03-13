525dde9ab74d569add8880e37291dcc17c474dca
aitorevi <aitorevi@gmail.com>
2023-03-13 09:55:58 +0100

    Backend finished

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import express from "express";
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello toma');
+app.get('/', async (req, res) => {  // Es necesario hacer asincrona la función para que espere a resolver el fetch antes de devolver la Url
+
+    const url = 'https://dog.ceo/api/breeds/image/random'
+    const dogUrl = await fetch(url)
+        .then(res => res.json()) // Convierte a JSON las respuesta que devuelve la API en texto plano
+        .then(json => json.message)  // Extrae del JSON la Url
+    res.send(JSON.stringify({url: dogUrl}) // parsea a String el objeto JSON
+    );
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
+
